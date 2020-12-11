a3cc9336af4f418784dcff103e2ee40bcf6ec252
Prakhar Mishra <pmprakhargenius@gmail.com>
2020-12-11 11:16:33 +0530

    Create preoreder_traversal.py

---
+++ tree/preoreder_traversal.py
@@ -0,0 +1,36 @@
+class Node:
+    def __init__(self, data=None):
+        self.data = data
+        self.left = None
+        self.right = None
+    
+    def insert(self, val):
+        if self.data:
+            if val < self.data:
+                if self.left is not None:
+                    self.left.insert(val)
+                else:
+                    self.left = Node(val)
+            else:
+                if self.right is not None:
+                    self.right.insert(val)
+                else:
+                    self.right = Node(val)
+        else:
+            self.data = data
+
+def preorder(root):
+    if root != None:
+        print (root.data)
+        preorder(root.left)
+        preorder(root.right)
+    
+    
+root = Node(20)
+root.insert(10)
+root.insert(6)
+root.insert(11)
+root.insert(22)
+root.insert(21)
+
+preorder(root)
