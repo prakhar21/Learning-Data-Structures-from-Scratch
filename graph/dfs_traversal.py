988fc431524f1ac2fd47fbdf33add7c9d6dc7c70
Prakhar Mishra <pmprakhargenius@gmail.com>
2021-02-10 18:45:17 +0530

    Create dfs_traversal.py

---
+++ graph/dfs_traversal.py
@@ -0,0 +1,27 @@
+
+def dfs(graph, start, visited=[]):
+    print (start)
+    visited.append(start)
+    neigh = graph[start]
+    for new_start in neigh:
+        if new_start in visited:
+            continue
+        else:
+            visited.append(new_start)
+            dfs(graph, new_start, visited)
+    return
+
+graph = {
+    '0': ['1', '2'],
+    '1': ['0', '3', '4'],
+    '2': ['0'],
+    '3': ['1'],
+    '4': ['2', '3', '5'],
+    '5': ['4']
+    }
+
+# 0 - 1 - 3
+# |   | /
+# 2 - 4 - 5
+
+dfs(graph, '0')
